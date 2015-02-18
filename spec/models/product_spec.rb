56f70c50f40546d5da92366d1f1c519b2f3cf61b
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-17 21:16:28 -0500

    Add First Unit Test For Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# we use "describe <class>" to start writing tests
+describe Product do
+  # Arrange necessary preconditions and inputs
+  let(:product) { create(:product) }
+
+  # we then write examples (specifications) in our describe block using an it-do-end syntax
+  # Example spec:
+  it "should have a name" do
+    # we instantiate a product object and assign it to "product"
+
+    ## we can use "should" to expect a value
+    expect(product.name).not_to be_nil
+
+    #be_valid runs the predicate matcher be_valid which is going to call the ActiveRecord valid? method
+    expect(product).to be_valid
+  end
+
+
+end
